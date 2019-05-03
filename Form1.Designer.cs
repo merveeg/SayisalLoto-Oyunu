5cd7dad8b4cdeee1ad07d0fc8f470de1374fe460
Merve Genç <38539228+merveeg@users.noreply.github.com>
2019-05-03 21:18:49 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace sayısalloto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toplam_para = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OYNA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(148, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(311, 54);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(148, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(311, 59);
+            this.panel2.TabIndex = 8;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(14, 37);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(53, 13);
+            this.lbl1.TabIndex = 9;
+            this.lbl1.Text = "Tahminler";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(14, 98);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(97, 13);
+            this.lbl2.TabIndex = 10;
+            this.lbl2.Text = "Kazanan Rakamlar";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(578, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 27);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Tuttur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(575, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_keyPress);
+            // 
+            // toplam_para
+            // 
+            this.toplam_para.Location = new System.Drawing.Point(37, 253);
+            this.toplam_para.Name = "toplam_para";
+            this.toplam_para.Size = new System.Drawing.Size(209, 20);
+            this.toplam_para.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(33, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 22);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "İkramiyeniz :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toplam_para);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox toplam_para;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayısalloto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int[] dizi = new int[6];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            oyunBaslat();
+        }
+        private void oyunBaslat()
+        {
+            int[] sayilar = new int[6];
+            
+
+            int solTaraf = 4;
+            panel1.Controls.Clear();
+            // Random rnd = new Random(DateTime.Now.Millisecond);
+            //int indis = rastgele.Next(1, 50);  //1 ile 49 arasında sayıları gecici diye bi degiskene atadım. 50 tane olur dizide  kaçıncıyı yazıca
+            //sayi dizimize rastgele sayı atayalım.
+
+            for (int i = 0; i < 6; i++)
+            {
+                TextBox txtbx = new TextBox();
+                panel1.Controls.Add(txtbx);
+                txtbx.Top = 30;
+                txtbx.Left = solTaraf;
+                txtbx.Width = 30;
+                txtbx.Text = "?";
+                txtbx.BackColor = Color.LightPink;
+                txtbx.Tag = sayilar[i];
+                solTaraf += 40;
+
+                txtbx.Refresh();
+
+
+            }
+            panel1.BackColor = Color.White;
+            panel1.Refresh();
+            int solTaraf1 = 4;
+            panel2.Controls.Clear();
+            for (int i = 0; i < 6; i++)
+            {
+                TextBox txtbx1 = new TextBox();
+                panel2.Controls.Add(txtbx1);
+                txtbx1.Top = 30;
+                txtbx1.Left = solTaraf1;
+                txtbx1.Width = 30;
+                txtbx1.Text = "_";
+                txtbx1.BackColor = Color.LightPink;
+                txtbx1.Tag = sayilar[i];
+                solTaraf1 += 40;
+
+                txtbx1.Refresh();
+            }
+            panel2.BackColor = Color.White;
+            panel2.Refresh();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var sayilar = new List<int>();
+
+            while (sayilar.Count < 6)
+            {
+                Random rastgele = new Random(DateTime.Now.Millisecond);
+                int sayi = rastgele.Next(1, 50);
+                if (!sayilar.Contains(sayi))
+                {
+                    sayilar.Add(sayi);
+
+                }
+            }
+            int i = 0;
+            foreach (var item in panel1.Controls)
+            {
+                if (item is TextBox)
+                {
+                    var txtbx1 = item as TextBox;
+                    if (txtbx1.Text == "?")
+                    {
+                        Random r = new Random();
+                        int sayi = r.Next(1, 50);
+                        txtbx1.Text = sayilar[i].ToString();
+                        i++;
+                    }
+                }
+            }
+
+            //diziyi yazdırabilirim burda
+            int bilinen = 0;
+            int index = 0;
+            foreach (var item in panel1.Controls)
+            {
+                if (item is TextBox)
+                {
+                    var txtbx1 = item as TextBox;
+                    if (int.Parse(txtbx1.Text) == dizi[index])
+                    {
+                        bilinen++;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+                }
+            }
+            MessageBox.Show("bilinen:" + bilinen);
+            int toplam = 0;
+            int para = 2;
+            switch (bilinen)
+            {
+                case 6:
+                    toplam += bilinen * para;
+                    toplam_para.Text = Convert.ToString(toplam) + " " + "₺";
+
+                    break;
+                case 5:
+                    toplam += bilinen * para;
+                    toplam_para.Text = Convert.ToString(para) + " " + "₺";
+                    break;
+
+                case 4:
+                    toplam += bilinen * para;
+                    toplam_para.Text = Convert.ToString(para) + " " + "₺";
+                    break;
+                case 3:
+                    toplam += bilinen * para;
+                    toplam_para.Text = Convert.ToString(para) + " " + "₺";
+                    break;
+                case 2:
+                    toplam += bilinen * para;
+                    toplam_para.Text = Convert.ToString(para) + " " + "₺";
+                    break;
+                case 1:
+                    toplam += bilinen * para;
+                    toplam_para.Text = Convert.ToString(para) + " " + "₺";
+                    break;
+                case 0:
+                    toplam += bilinen * para;
+                    toplam_para.Text = "Malesef Hiç Bir şey Kazanamadınız :( ";
+                    MessageBox.Show("Oyun bitti");
+                    break;
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool isEnd = true;
+            foreach (Control item in panel2.Controls)
+            {
+                if (item is TextBox)
+
+                {
+                    var txtbx = item as TextBox;
+                    if (item.Text == textBox1.Text)
+                        isEnd = false;
+                }
+            }
+            if (!isEnd)
+            {
+                MessageBox.Show("Bu değer daha önce girildi");
+                return;
+            }
+            var deger = Convert.ToInt32(textBox1.Text);
+                if (deger > 49)
+
+                {
+                    MessageBox.Show("Lütfen 1-49 arası sayi girin:");
+                    return;
+                }
+                else if (deger <= 0)
+                {
+                    MessageBox.Show("negatif veya 0 deger girilemez.");
+                    return;
+                }
+
+                int sayac = 0;
+                foreach (var item in panel2.Controls)
+                {
+                    if (item is TextBox)
+
+                    {
+                        var txtbx = item as TextBox;
+
+                        if (txtbx.Text == "_")
+                        {
+                            txtbx.Text = textBox1.Text;
+                            dizi[sayac] = int.Parse(textBox1.Text);
+
+                            textBox1.Clear();
+                            return;
+                        }
+                        else //değilse sayacı arttırsın
+                        {
+                            sayac++;
+                        }
+
+                    }
+
+                }
+
+
+
+
+            }
+
+            private void textBox1_keyPress(object sender, KeyPressEventArgs e)
+            {
+
+                if (e.KeyChar == (char)Keys.Enter)
+                {
+                    e.Handled = true;
+                    button2.PerformClick();
+                }
+            }
+        }
+    }
+
+
+
